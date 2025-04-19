6bc64abb54f22efbb50e91180de0bc75c7441a55
vibhuti2006 <vibhutibvbvpn0823@gmail.com>
2025-04-19 15:44:06 +0530

    Sidebar base code

---
+++ src/assets/assets.js
@@ -0,0 +1,30 @@
+import code_icon from './code_icon.png'
+import compass_icon from './compass_icon.png'
+import gallery_add_icon from './gallery_add_icon.png'
+import gemini_icon from './gemini_icon.png'
+import hamburger_menu from './hamburger_menu.png'
+import history_icon from './history_icon.png'
+import light_bulb from './light_bulb.png'
+import message_icon from './message_icon.png'
+import mic_icon from './mic_icon.png'
+import plus_icon from './plus_icon.png'
+import question_icon from './question_icon.png'
+import send_icon from './send_icon.png'
+import settings_icon from './settings_icon.png'
+
+
+export const assets={
+    code_icon,
+    compass_icon,
+    gallery_add_icon,
+    gemini_icon,
+    hamburger_menu,
+    history_icon,
+    light_bulb,
+    message_icon,
+    mic_icon,
+    plus_icon,
+    question_icon,
+    send_icon,
+    settings_icon
+}
